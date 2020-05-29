23b6698a656a1e07c4121853a2e37a25aeaa0252
DESKTOP-150EU5N\Kamil <k.musial@interactivevision.pl>
2020-05-29 12:33:47 +0200

    Updates

---
+++ precache-manifest.de310d5d3dedf2dac4d4a55d093696d6.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13251cb09d90574c74328fc4147cc4c7",
+    "url": "/calendar/index.html"
+  },
+  {
+    "revision": "c5e7163309e7979e464b",
+    "url": "/calendar/static/css/2.cdc8b915.chunk.css"
+  },
+  {
+    "revision": "cc5e1e203fdaead58b56",
+    "url": "/calendar/static/css/main.3e24ff23.chunk.css"
+  },
+  {
+    "revision": "c5e7163309e7979e464b",
+    "url": "/calendar/static/js/2.58f0fbb3.chunk.js"
+  },
+  {
+    "revision": "19ec9142bc4900454413e3416a6204ce",
+    "url": "/calendar/static/js/2.58f0fbb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc5e1e203fdaead58b56",
+    "url": "/calendar/static/js/main.208e70fb.chunk.js"
+  },
+  {
+    "revision": "4988624ce27ff200b9ab",
+    "url": "/calendar/static/js/runtime-main.c2818047.js"
+  },
+  {
+    "revision": "a5f352910cdb605652f335e2c85abd93",
+    "url": "/calendar/static/media/bell.a5f35291.svg"
+  },
+  {
+    "revision": "57f73f9d83341e72c1c1020e990406a6",
+    "url": "/calendar/static/media/calendar.57f73f9d.svg"
+  },
+  {
+    "revision": "4b93446cf9abb836dbe76e3d0b5e0e65",
+    "url": "/calendar/static/media/checked.4b93446c.svg"
+  },
+  {
+    "revision": "a627cbb969ae417b0c50a63766e260f2",
+    "url": "/calendar/static/media/close.a627cbb9.svg"
+  },
+  {
+    "revision": "db578cb88efbcd512ae7a33da79e6306",
+    "url": "/calendar/static/media/interactivelogo.db578cb8.svg"
+  },
+  {
+    "revision": "0969c92b498df082b2f18fc455b2fa13",
+    "url": "/calendar/static/media/logout.0969c92b.svg"
+  },
+  {
+    "revision": "a14461b86078261a696a425d13ffe612",
+    "url": "/calendar/static/media/magnifier.a14461b8.svg"
+  },
+  {
+    "revision": "9b405986ce6b58dd1d474c4b59cd2548",
+    "url": "/calendar/static/media/moon.9b405986.svg"
+  },
+  {
+    "revision": "6e10627c38977fc1e5f52aef932a6f5a",
+    "url": "/calendar/static/media/poland.6e10627c.svg"
+  },
+  {
+    "revision": "c77d0869734069a0e5c4b37829d2cfdb",
+    "url": "/calendar/static/media/question.c77d0869.svg"
+  },
+  {
+    "revision": "a7ade154f3ac026edd6f44fbedea6889",
+    "url": "/calendar/static/media/settings.a7ade154.svg"
+  },
+  {
+    "revision": "17baa72f0e34fa68885bfd9a89ca4cfe",
+    "url": "/calendar/static/media/statistics.17baa72f.svg"
+  },
+  {
+    "revision": "49bfb57fe009003f2cd666306e9afc79",
+    "url": "/calendar/static/media/sunny.49bfb57f.svg"
+  },
+  {
+    "revision": "e5564902e2642c5e6e2e98e68a7d41f5",
+    "url": "/calendar/static/media/uk.e5564902.svg"
+  }
+]);
